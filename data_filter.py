3dd796c9bdea6d9d1fb887c00b3a78b822dfcf4e
Miguel Ruiz <miguel.ruizmn@gmail.com>
2022-05-14 13:36:30 -0500

    Modifiqué workers in data filter

--- data_filter.py
+++ data_filter.py
@@ -74,8 +74,16 @@ DATA = [
 def run():
     all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
     all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    
+        
     for worker in all_python_devs:
-        print(all_python_devs)  
+        print(worker) 
+    
+    
+    for workers in all_platzi_workers:
+        print(workers) 
+
+
     adults = list(filter(lambda worker: worker["age"] > 18,DATA))
     adults = list(map(lambda worker: worker["name"],adults))
     old_people = list(map(lambda worker: worker | {"old":worker["age"] > 70}, DATA))
