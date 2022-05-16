c8355f0ac7d93ed85ee0f8a22e30fdbeef7331ec
Miguel Ruiz <miguel.ruizmn@gmail.com>
2022-05-16 13:49:54 -0500

    Terminé try, except, finally, and raise, Falta repasar el caso raise y finally

--- debugging.py
+++ debugging.py
@@ -1,15 +1,17 @@
 def divisors(num):
     divisors = []
     for i in range(1, num + 1):
-        if num % i == 1:
+        if num % i == 0:
             divisors.append(i)
     return divisors
 
-
 def run():
-    num = int(input("Introduce un número: "))
-    print(divisors(num))
-    print("Termino mi programa")
+    try:
+        num = int(input("Introduce un número: "))
+        print(divisors(num))
+        print("Termino mi programa")
+    except ValueError:
+        print("Solo se permiten números")
 
 if __name__ == "__main__":
     run()
