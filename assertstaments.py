90b5cbfba11d022e5a34435b1851299b82afea02
Miguel Ruiz <miguel.ruizmn@gmail.com>
2022-05-16 13:56:42 -0500

    Empecé la clase de assertsStatemnt

---
+++ assertstaments.py
@@ -0,0 +1,15 @@
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+def run():
+    num = input("Introduce un número: ")
+    assert num.isnumeric(), "Solo se permiten números"
+    print(divisors(int(num)))
+    print("Termino mi programa")
+
+if __name__ == "__main__":
+    run()
